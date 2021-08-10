c0974da8bcce83b704131e5950ed7d3b4d96e8c2
Jarin Tasnim Shama <84661428+Jarintasnimshama@users.noreply.github.com>
2021-08-11 01:38:26 +0600

    Update CommentSection.php

--- views/Blogger/CommentSection.php
+++ views/Blogger/CommentSection.php
@@ -1,8 +1,8 @@
 <div style="width: calc(100%)">
-    <div style="width: calc(90%);float:left;">
+    <div style="width: calc(80%);float:left;">
         <input type="text" class="inputText1" name="commentTB" id="commentTB" placeholder="Write Comment Here...">
     </div>
-    <div style="width: calc(9%);float:left;" align="left">
+    <div style="width: calc(20%);float:left;" align="center">
         <button name="commentBTN" id="commentBTN" class="btnLogin">Comment</button>
     </div>
 </div>
