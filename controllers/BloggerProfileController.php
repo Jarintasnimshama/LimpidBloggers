8237c690e30f54f4daed4f60829fa2dc84cdd13d
Jarin Tasnim Shama <84661428+Jarintasnimshama@users.noreply.github.com>
2021-08-15 19:52:34 +0600

    Update BloggerProfileController.php

--- controllers/BloggerProfileController.php
+++ controllers/BloggerProfileController.php
@@ -61,33 +61,40 @@ function loadBloggersPostsMyPanel()
     $Sl = 1;
     if(sizeof($allData) > 0 && $allData != null)
     {
-        foreach($allData as $data)
+        if($allData[0]['title']!=null)
         {
-            $content .= '<tr>';
-            $content .=        '<td>'.$Sl.'</td>';
-            $content .=        '<td>'.$data["title"].'</td>';
-            $content .=        '<td>'.$data["category"].'</td>';
-            $content .=        '<td>'.$data["post_time"].'</td>';
-            $content .=        '<td><span class="attenText1">'.$data["blog_status"].'</span></td>';
-            $content .=        '<td>'.$data["comment_count"].'</td>';
-            $content .=        '<td>'.$data["bookmark_count"].'</td>';
-            if($data['blog_status'] == "Approved")
+            foreach($allData as $data)
             {
-                $content .=        '<td align="middle">';
-                $content .=                '<a class="linkBtn1" href="https://localhost/LimpidBloggers/views/Common/Blog.php?id='.$data["blog_id"].'">View Blog</a>&nbsp;';
-                $content .=                '<a class="linkBtn3" href="https://localhost/LimpidBloggers/views/Blogger/UpdateMyBlog.php?id='.$data["blog_id"].'">Update</a>&nbsp';
-                $content .=                '<a class="linkBtn4" onclick="return confirm_delete();" href="https://localhost/LimpidBloggers/controllers/api/DeleteMyBlog.php?id='.$data["blog_id"].'">Delete</a>';
-                $content .=        '</td>';
+                $content .= '<tr>';
+                $content .=        '<td>'.$Sl.'</td>';
+                $content .=        '<td>'.$data["title"].'</td>';
+                $content .=        '<td>'.$data["category"].'</td>';
+                $content .=        '<td>'.$data["post_time"].'</td>';
+                $content .=        '<td><span class="attenText1">'.$data["blog_status"].'</span></td>';
+                $content .=        '<td>'.$data["comment_count"].'</td>';
+                $content .=        '<td>'.$data["bookmark_count"].'</td>';
+                if($data['blog_status'] == "Approved")
+                {
+                    $content .=        '<td align="middle">';
+                    $content .=                '<a class="linkBtn1" href="https://localhost/LimpidBloggers/views/Common/Blog.php?id='.$data["blog_id"].'">View Blog</a>&nbsp;';
+                    $content .=                '<a class="linkBtn3" href="https://localhost/LimpidBloggers/views/Blogger/UpdateMyBlog.php?id='.$data["blog_id"].'">Update</a>&nbsp';
+                    $content .=                '<a class="linkBtn4" onclick="return confirm_delete();" href="https://localhost/LimpidBloggers/controllers/api/DeleteMyBlog.php?id='.$data["blog_id"].'">Delete</a>';
+                    $content .=        '</td>';
+                }
+                else
+                {
+                    $content .=        '<td align="middle">';
+                    $content .=                '<a class="linkBtn3" href="https://localhost/LimpidBloggers/views/Blogger/UpdateMyBlog.php?id='.$data["blog_id"].'">Update</a>&nbsp';
+                    $content .=                '<a class="linkBtn4" onclick="return confirm_delete();" href="https://localhost/LimpidBloggers/controllers/api/DeleteMyBlog.php?id='.$data["blog_id"].'">Delete</a>';
+                    $content .=        '</td>';
+                }
+                $content .='</tr>';	
+                $Sl += 1;
             }
-            else
-            {
-                $content .=        '<td align="middle">';
-                $content .=                '<a class="linkBtn3" href="https://localhost/LimpidBloggers/views/Blogger/UpdateMyBlog.php?id='.$data["blog_id"].'">Update</a>&nbsp';
-                $content .=                '<a class="linkBtn4" onclick="return confirm_delete();" href="https://localhost/LimpidBloggers/controllers/api/DeleteMyBlog.php?id='.$data["blog_id"].'">Delete</a>';
-                $content .=        '</td>';
-            }
-            $content .='</tr>';	
-            $Sl += 1;
+        }
+        else
+        {
+            $content .= '<tr><td colspan="6" align="middle"><h2 class="error2">NO BLOGS FOUND.</h2></td></tr>';
         }
     }
     else
